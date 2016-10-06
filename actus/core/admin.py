60a0358611bb75fa0bed8901312e26d72884358c
Paulo Romano <pauloromanocarvalho@gmail.com>
2016-10-06 08:39:09 -0300

    Add Problem admin

--- actus/core/admin.py
+++ actus/core/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from actus.core.models import Problem
+
+class BaseModelAdmin(admin.ModelAdmin):
+    exclude = ('created_at', 'created_by', 'updated_at', 'updated_by')
+
+
+@admin.register(Problem)
+class ProblemAdmin(BaseModelAdmin):
+    short_description = 'Problemas'
+    fields = ('name',)
+    search_fields = ('name',)
+    list_display = ('name',)
+
