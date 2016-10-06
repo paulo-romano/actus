32edbc269f3c2e8dccf4536204c4181bef2960da
Paulo Romano <pauloromanocarvalho@gmail.com>
2016-10-06 08:34:07 -0300

    Problem model

--- actus/core/models.py
+++ actus/core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class BaseModel(models.Model):
+    from uuid import uuid4
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+
+    class Meta:
+        abstract = True
+
+
+class Problem(BaseModel):
+    name = models.CharField(verbose_name='Nome', max_length=100)
+
+    class Meta:
+        verbose_name = 'problema'
+        verbose_name_plural = 'problemas'
