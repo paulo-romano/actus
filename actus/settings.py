14110137a2a8154efb40982cde20b67ff4b56ad3
Paulo Romano <pauloromanocarvalho@gmail.com>
2016-12-11 12:28:54 -0200

    Visualização de todas as notificações
    Para finalizar o sisteam de notificacoes requisitado em #8

--- actus/settings.py
+++ actus/settings.py
@@ -22,8 +22,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_gravatar',
-    'notifications',
     'actus.core',
+    'notifications',
 ]
 
 MIDDLEWARE = [
